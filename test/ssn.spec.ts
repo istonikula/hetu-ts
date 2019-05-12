b5be5c54af9993384d50eb887ecfb186f111c79a
Isto Nikula <isto.nikula@iki.fi>
2019-05-12 09:41:07 +0300

    feat(api): clean up api

--- test/ssn.spec.ts
+++ test/ssn.spec.ts
@@ -7,89 +7,76 @@ describe('ssn', () => {
   const bday2000 = LocalDate.of(2000, 11, 12)
 
   it('female', () => {
-    expect(ssn.Parser.parse('210721-1703').isFemale()).toBeTruthy()
-
-    const actual = ssn.Parser.parse(`${ssn.female(bday1900)}`)
-    expect(actual.isFemale()).toBeTruthy()
-    expect(actual.isTemporal()).toBeFalsy()
+    verifyFemale(ssn.parse('210721-1703'))
+    verifyFemale(ssn.parse(`${ssn.female(bday1900)}`))
+    verifyFemale(ssn.parse(`${ssn.female()}`))
   })
 
   it('male', () => {
-    expect(ssn.Parser.parse('260785-309A').isFemale()).toBeFalsy()
-
-    const actual = ssn.Parser.parse(`${ssn.male(bday2000)}`)
-    expect(actual.isFemale()).toBeFalsy()
-    expect(actual.isTemporal()).toBeFalsy()
+    verifyMale(ssn.parse('260785-309A'))
+    verifyMale(ssn.parse(`${ssn.male(bday2000)}`))
+    verifyMale(ssn.parse(`${ssn.male()}`))
   })
 
   it('female temporal', () => {
-    expect(ssn.Parser.parse('090707-998E').isFemale()).toBeTruthy()
-
-    const actual = ssn.Parser.parse(`${ssn.femaleTemporal(bday2000)}`)
-    expect(actual.isFemale()).toBeTruthy()
-    expect(actual.isTemporal()).toBeTruthy()
+    verifyFemaleTemporal(ssn.parse('090707-998E'))
+    verifyFemaleTemporal(ssn.parse(`${ssn.femaleTemporal(bday2000)}`))
+    verifyFemaleTemporal(ssn.parse(`${ssn.femaleTemporal()}`))
   })
 
   it('male temporal', () => {
-    expect(ssn.Parser.parse('050162-981N').isFemale()).toBeFalsy()
-
-    const actual = ssn.Parser.parse(`${ssn.maleTemporal(bday1900)}`)
-    expect(actual.isFemale()).toBeFalsy()
-    expect(actual.isTemporal()).toBeTruthy()
+    verifyMaleTemporal(ssn.parse('050162-981N'))
+    verifyMaleTemporal(ssn.parse(`${ssn.maleTemporal(bday1900)}`))
+    verifyMaleTemporal(ssn.parse(`${ssn.maleTemporal()}`))
   })
 
   it('bday', () => {
     const nonDate = 'xxxxxx-450F'
-    expect(() => ssn.Parser.parse(nonDate)).toThrowError(
-      'Invalid bday: pattern mismatch',
-    )
+    expect(() => ssn.parse(nonDate)).toThrowError('Invalid bday: pattern mismatch')
   })
 
   it('century id', () => {
-    const valid = ssn.Parser.parse('220305A244S')
+    const valid = ssn.parse('220305A244S')
 
     const lowercase = `${valid.bday}a${valid.nnn}${valid.control}`
-    expect(() => ssn.Parser.parse(lowercase)).toThrowError(
-      'Unsupported century id a',
-    )
+    expect(() => ssn.parse(lowercase)).toThrowError('Unsupported century id a')
 
     const notSupported = '090521+9220'
-    expect(() => ssn.Parser.parse(notSupported)).toThrowError(
-      'Unsupported century id +',
-    )
+    expect(() => ssn.parse(notSupported)).toThrowError('Unsupported century id +')
   })
 
   it('nnn', () => {
-    expect(() => ssn.Parser.parse('020146-000F')).toThrowError(
-      'Invalid nnn: 000',
-    )
+    expect(() => ssn.parse('020146-000F')).toThrowError('Invalid nnn: 000')
   })
 
   it('control char', () => {
-    const valid = ssn.Parser.parse('020146-450F')
+    const valid = ssn.parse('020146-450F')
 
     const lowercase = `${valid.bday}-${valid.nnn}f`
-    expect(() => ssn.Parser.parse(lowercase)).toThrowError(
-      'Invalid ssn: control char mismatch',
-    )
+    expect(() => ssn.parse(lowercase)).toThrowError('Invalid ssn: control char mismatch')
 
     const realMismatch = `${valid.bday}-${valid.nnn}A`
-    expect(() => ssn.Parser.parse(realMismatch)).toThrowError(
-      'Invalid ssn: control char mismatch',
-    )
+    expect(() => ssn.parse(realMismatch)).toThrowError('Invalid ssn: control char mismatch')
   })
 
   it('pattern: too short', () => {
     const tooShort = `${ssn.male()}`.substring(1)
-    expect(() => ssn.Parser.parse(tooShort)).toThrowError(
-      'Invalid ssn: pattern mismatch',
-    )
+    expect(() => ssn.parse(tooShort)).toThrowError('Invalid ssn: pattern mismatch')
   })
 
   it('pattern: too long', () => {
     const tooLong = ssn.male() + 'X'
-    expect(() => ssn.Parser.parse(tooLong)).toThrowError(
-      'Invalid ssn: pattern mismatch',
-    )
+    expect(() => ssn.parse(tooLong)).toThrowError('Invalid ssn: pattern mismatch')
   })
 })
+
+const verifySsn = (isFemale: boolean, isTemporal: boolean) => (actual: ssn.ValidSsn) => {
+  expect(actual.isFemale()).toEqual(isFemale)
+  expect(actual.isMale()).toEqual(!isFemale)
+  expect(actual.isTemporal()).toEqual(isTemporal)
+}
+
+const verifyFemale = verifySsn(true, false)
+const verifyFemaleTemporal = verifySsn(true, true)
+const verifyMale = verifySsn(false, false)
+const verifyMaleTemporal = verifySsn(false, true)
